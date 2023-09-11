99da3cee072cf7def5ae5192c980b49d97b7f1c3
Campbell <campbell.gilbert@gmail.com>
2023-09-11 10:57:24 -0700

    Add project files.

---
+++ ravendesk/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace ravendesk
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveClicked(object sender, EventArgs e)
+        {
+            string text = textEditor.Text;
+            File.WriteAllText("savedDocument.txt", text);
+        }
+
+        /*private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }*/
+    }
+}
