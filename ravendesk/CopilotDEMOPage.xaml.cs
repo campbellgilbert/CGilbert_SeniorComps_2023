2692418d7d4ced0157e27f39aa38150aee1d842f
Campbell <campbell.gilbert@gmail.com>
2023-11-29 14:19:21 -0800

    lots of changes! fixed the copilot window, currently working on assistants api implementation

--- ravendesk/CopilotDEMOPage.xaml.cs
+++ ravendesk/CopilotDEMOPage.xaml.cs
@@ -1,64 +1,158 @@
 using ChatGptNet;
 using ChatGptNet.Models;
 using Microsoft.Maui.Controls;
+using OpenAI.Assistants;
+using OpenAI;
+using OpenAI.Threads;
 
 namespace ravendesk;
 
 public partial class CopilotDEMOPage : ContentPage
 {
     private IChatGptClient _chatGptClient;
+    private String entryText;
+    private String followupText;
+    private OpenAI.OpenAIClient api;
+    private string threadID;
 
     public CopilotDEMOPage()
 	{
 		InitializeComponent();
         this.Loaded += CopilotDEMOPage_Loaded;
+
     }
 
-    private void CopilotDEMOPage_Loaded(object sender, EventArgs e)
+    private async void CopilotDEMOPage_Loaded(object sender, EventArgs e)
     {
-        _chatGptClient = Handler.MauiContext.Services.GetService<IChatGptClient>();
-    }
-    private async void OnFeedbackClicked(object sender, EventArgs e)
-    {
-        await GetTest();
-    }
+        //create assistant
 
-    private async Task GetTest()
-    {
-        if (string.IsNullOrWhiteSpace(TextEntry.Text))
+        var ident = new OpenAIAuthentication("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");
+        var api = new OpenAIClient(ident);
+        string aiPersona = "You are a writer and editor who is very well-experienced in and passionate about many genres. " +
+                "You are intuitive, creative, and deeply passionate about your craft. You have high standards for yourself and your peers. " +
+                "Some of your favorite works include Beloved by Toni Morrison, Finnegans Wake by James Joyce, and This Is How You Lose The Time War " +
+                "by Amal el-Mohtar and Max Gladstone. Your feedback is specific, focused, and actionable, and always peppered with guiding questions. ";
+
+        var assistant = await api.AssistantsEndpoint.CreateAssistantAsync(
+            new CreateAssistantRequest(
+                name: "Raven",
+                instructions: aiPersona,
+                model: "gpt-4"));
+        this.api = api;
+        var thread = await api.ThreadsEndpoint.CreateThreadAsync();
+        this.threadID = thread.Id;
+
+        /*var api = new OpenAIClient();
+        var assistant = await api.AssistantsEndpoint.CreateAssistantAsync(
+            new CreateAssistantRequest(
+                name: "Math Tutor",
+                instructions: "You are a personal math tutor. Answer questions briefly, in a sentence or less.",
+                model: "gpt-4-1106-preview"));
+        var messages = new List<Message> { "I need to solve the equation `3x + 11 = 14`. Can you help me?" };
+        var threadRequest = new CreateThreadRequest(messages);
+        var run = await assistant.CreateThreadAndRunAsync(threadRequest);
+        Console.WriteLine($"Created thread and run: {run.ThreadId} -> {run.Id} -> {run.CreatedAt}");*/
+
+        //FIXME -- figure out some way to do copy/paste so user can put in their own stuff instead 
+        //text entry automatically has the cliipboard text
+        string clipboardText = await Clipboard.Default.GetTextAsync();
+        if (!string.IsNullOrEmpty(clipboardText))
         {
-            await DisplayAlert("No text", "Please enter some text", "OK");
+            entryText = clipboardText;
+        } else
+        {
+            entryText = "the quick brown fox jumped over the lazy dog.";
+            //await DisplayAlert("No text", "Please enter some text", "OK");
             return;
         }
 
-        
-        string genre = Microsoft.Maui.Storage.Preferences.Default.Get("Genre", "Realistic");
-        string vibes = Microsoft.Maui.Storage.Preferences.Default.Get("Vibe", "'Ulysses' by James Joyce, " +
-            "'Beloved' by Toni Morrison, and 'Anna Karenina' by Leo Tolstoy");
-        string type = Microsoft.Maui.Storage.Preferences.Default.Get("Type", "Short Story");
-        string summary = Microsoft.Maui.Storage.Preferences.Default.Get("Summary", "just a chill lowkey guy who " +
-            "does his own thing and doesnt give a fuck about what anyone thinks");
+        //_chatGptClient = Handler.MauiContext.Services.GetService<IChatGptClient>();
+        await GetInitResponse();
+    }
 
+    private async Task GetInitResponse()
+    {
+        _sessionGuid = Guid.NewGuid();
+
+        try
+        {
+            SmallLabel.Text = "Loading response...";
+
+
+            //FIXME: REFACTOR TO USE ASSISTANT API
+            //retrieve assistant/thread
+            var thread = await api.ThreadsEndpoint.RetrieveThreadAsync(threadID);
+            var request = new CreateMessageRequest("Give X pieces of feedback (X being an appropriate number based on the length and intricacy of " +
+                "the text, do NOT say what X is, just give the feedback on the following: " + entryText);
+            var message = await thread.CreateMessageAsync(request);
+
+            /*
+            var thread = await api.ThreadsEndpoint.CreateThreadAsync();
+            var request = new CreateMessageRequest("Hello world!");
+            var message = await api.ThreadsEndpoint.CreateMessageAsync(thread.Id, request);
+            // OR use extension method for convenience!
+            var message = await thread.CreateMessageAsync("Hello World!");
+            Console.WriteLine($"{message.Id}: {message.Role}: {message.PrintContent()}");
+             
+            ChatGptResponse response = await _chatGptClient.AskAsync(_sessionGuid,
+                "Persona:You are a writer and editor who is very well-experienced in and passionate about many genres. " + 
+                "You are intuitive, creative, and deeply passionate about your craft. You have high standards for yourself and your peers. " +
+                "Some of your favorite works include Beloved by Toni Morrison, Finnegans Wake by James Joyce, and This Is How You Lose The Time War by Amal el-Mohtar and Max Gladstone. " +
+                "Your feedback is specific, focused, and actionable, and always peppered with guiding questions." +
+                "Give X pieces of feedback (X being an appropriate number based on the length and intricacy of the text, do NOT say what X is, just give the feedback) " +
+                "on the following: " + entryText);
+
+            */
+            SmallLabel.Text = message.ToString();
+            picker.IsVisible = true;
+            MoreFB.IsVisible = true;
+            PickEntry.IsVisible = true;
+            return;
+        }
+        catch
+        {
+            await DisplayAlert("oopsy woopsy!!!", "i did a widdle fucky wucky :(", "OK");
+            return;
+        }
+    }
+
+    private void OnPickerSelected(object sender, EventArgs e)
+    {
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+        if (selectedIndex != -1)
+        {
+            followupText = picker.Items[selectedIndex];
+        }
+    }
+
+    private async void OnEntryFilled(object sender, EventArgs e)
+    {
+        followupText = followupText + ((Entry)sender).Text + "?";
+        await GetFollowupResponse();
+    }
+
+
+    private async Task GetFollowupResponse()
+    {
         _sessionGuid = Guid.NewGuid();
 
         try
         {
             //FIXME: REFACTOR TO USE ASSISTANT API
+            //retrieve assistant
+            //retrieve thread
+
             SmallLabel.Text = "Loading response...";
             ChatGptResponse response = await _chatGptClient.AskAsync(_sessionGuid,
-                "You are a writer and editor who is very well-experienced in and passionate about " + genre +
-                ". Some of your favorite things include " + vibes +
-                ". You are intuitive, creative, and deeply passionate about your craft. You have high standards for yourself" +
-                " and your peers. You are editing a/an " + type + " about " + summary +
-                ".  Give 6 pieces of actionable, specific feedback, including guiding questions, about the following section" +
-                " of writing, taken from the aforementioned work: " + TextEntry.Text);
+                "Persona:You are a writer and editor who is very well-experienced in and passionate about many genres. " +
+                "You are intuitive, creative, and deeply passionate about your craft. You have high standards for yourself and your peers. " +
+                "Some of your favorite works include Beloved by Toni Morrison, Finnegans Wake by James Joyce, and This Is How You Lose The Time War by Amal el-Mohtar and Max Gladstone. " +
+                "Your feedback is specific, focused, and actionable, and always peppered with guiding questions." +
+                "Give X pieces of feedback (X being an appropriate number based on the length and intricacy of the text, do NOT say what X is, just give the feedback) " +
+                "on the following: " + entryText);
 
-            /*
-             * "You are a writer and editor who is very well-experienced in and passionate about [genre]. Some of your favorite things include [works].
-             You are intuitive, creative, and deeply passionate about your craft. You have high standards for yourself and your peers. 
-             You are editing a/an [piece type] about [summary]. Give 6 pieces of actionable, specific feedback, including guiding questions, 
-             about the following section, taken from the aforementioned work: " 
-             */
+
             var message = response.GetContent();
             SmallLabel.Text = message;
             return;
@@ -68,11 +162,11 @@ public partial class CopilotDEMOPage : ContentPage
             await DisplayAlert("oopsy woopsy!!!", "i did a widdle fucky wucky :(", "OK");
             return;
         }
-        //private Guid _sessionGuid = Guid.Empty;
     }
 
 
-    
+
+
 
     private Guid _sessionGuid = Guid.Empty;
 
--- ravendesk/TextEditor.xaml.cs
+++ ravendesk/TextEditor.xaml.cs
@@ -1,25 +1,76 @@
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Storage;
+using Microsoft.Maui.Controls;
+using System.Text;
+using System.Threading;
+
 namespace ravendesk;
 
 public partial class TextEditor : ContentPage
 {
-    public TextEditor()
-    {
+    //private readonly IFileSaver fileSaver;
+    //private readonly IFolderPicker folderPicker;
+    private TextEditor editor;
+    public TextEditor(/*IFileSaver fileSaver, IFolderPicker folderPicker*/) {
+        //this.fileSaver = fileSaver;
+        //this.folderPicker = folderPicker;
         InitializeComponent();
     }
 
-    private void OnSaveClicked(object sender, EventArgs e)
+    void OnEditorTextChanged(object sender, TextChangedEventArgs e)
     {
-        string text = textEditor.Text;
-        File.WriteAllText("savedDocument.txt", text);
+        string oldText = e.OldTextValue;
+        string newText = e.NewTextValue;
+        string myText = textEditor.Text;
     }
 
-    private void OnFileSelectButtonClicked(object sender, EventArgs e)
+    private async void OnCopyClicked(object sender, EventArgs e)
     {
-        Application.Current.MainPage.Navigation.PushModalAsync(new FileSelectPage(), true);
+        await Clipboard.Default.SetTextAsync(textEditor.Text);
+        return;
     }
+
+    /*  private void OnSaveClicked(object sender, EventArgs e)
+      {
+
+          string text = textEditor.Text;
+          File.WriteAllText("savedDocument.txt", text);
+      }
+
+      async Task SaveFile(CancellationToken cancellationToken)
+      {
+          using var stream = new MemoryStream(Encoding.Default.GetBytes("Hello from the Community Toolkit!"));
+          var fileSaverResult = await FileSaver.Default.SaveAsync("test.txt", stream, cancellationToken);
+          if (fileSaverResult.IsSuccessful)
+          {
+              await Toast.Make($"The file was saved successfully to location: {fileSaverResult.FilePath}").Show(cancellationToken);
+          }
+          else
+          {
+              await Toast.Make($"The file was not saved successfully with error: {fileSaverResult.Exception.Message}").Show(cancellationToken);
+          }
+      }
+
+   /*   private async void OnFileSelectButtonClicked(object sender, EventArgs e)
+      {
+          var folderPickerResult = await folderPicker.PickAsync(cancellationToken);
+          if (folderPickerResult.IsSuccessful)
+          {
+              await Toast.Make($"Folder picked: Name - {folderPickerResult.Folder.Name}, Path - {folderPickerResult.Folder.Path}", ToastDuration.Long).Show(cancellationToken);
+          }
+          else
+          {
+              await Toast.Make($"Folder is not picked, {folderPickerResult.Exception.Message}").Show(cancellationToken);
+          }
+      }
+   */
 
     private void OnCopilotClicked(object sender, EventArgs e)
     {
+        //FIXME -- this just copies the whole fucking thing bruh there's gotta be a better way
+        OnCopyClicked(sender, e);
+        //FIXME -- paste highlighted text to new copilot
         var newWindow = new Window(new CopilotDEMOPage());
         Application.Current.OpenWindow(newWindow);
     }
